3d7f5eabb6194d4db51428bc6a57bcdf0f9e9c2c
Daniel Rivera <danielf.r97@gmail.com>
2018-09-06 20:45:45 -0500

    Imagen con transparencia

--- musica.m
+++ musica.m
@@ -1,3 +1,9 @@
 clear all;close all;clc;
-[y,Fs] = audioread('InitialD-dejavu.mp3'); sound(y,Fs);
-sound(y);
+% [y,Fs] = audioread('InitialD-dejavu.mp3'); sound(y,Fs);
+% sound(y);
+[A,map,transparency] =imread('Guitarra.png');
+[A1,map1,transparency1]=imread('me.png');
+f=imshow(A);impixelinfo;set(f, 'AlphaData', transparency);
+hold on
+G=image(355,216,A1);set(G, 'AlphaData', transparency1);
+
